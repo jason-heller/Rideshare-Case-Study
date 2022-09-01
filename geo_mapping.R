aa902ef04f54f090f3dae2fd1136e08d55501292
jheller9 <jasonheller96@gmail.com>
2022-09-01 19:24:30 -0400

    Line graphs, removed titles from graphs

--- geo_mapping.R
+++ geo_mapping.R
@@ -1,5 +1,11 @@
 geodata = st_as_sf(tripdata, coords = c(tripdata$start_lat, tripdata$start_lng))
 geodata = st_set_crs(tripdata, crs = 4326)
 
-ggplot(geodata) + geom_sf(aes(color = cluster))
+#ggplot(geodata) + geom_sf(aes(color = cluster))
 # TODO: Graph stations points and make heatmap
+
+michigan <- map_data("state", region = "michigan")
+
+ggplot(data = michigan, aes(x = tripdata$start_lng, y = tripdata$start_lat, group = group)) +
+  geom_polygon(fill="lightgray", color = "white")+
+  geom_point(data = df, aes(x = tripdata$start_lng, y = tripdata$start_lat, group = name), color="red")
