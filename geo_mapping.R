db55a07ee8011181ce5eedf195ec32197b7e4c8a
jheller9 <jasonheller96@gmail.com>
2022-09-01 22:44:56 -0400

    Line graphs, removed titles from graphs

--- geo_mapping.R
+++ geo_mapping.R
@@ -1,12 +1,12 @@
-lat_min = min(tripdata$start_lat)
-lat_max = max(tripdata$start_lat)
-lng_min = min(tripdata$start_lng)
-lng_max = max(tripdata$start_lng)
+#lat_min = min(tripdata$start_lat)
+#lat_max = max(tripdata$start_lat)
+#lng_min = min(tripdata$start_lng)
+#lng_max = max(tripdata$start_lng)
 states = map_data("state", region = "illinois")
 
 ggplot(states, aes(long, lat)) +
 geom_polygon(aes(group = group)) +
-coord_map("albers",  lat0 = 45.5, lat1 = 29.5) +
-st_crop(worldmap, xmin = lng_min, xmax = lng_max, ymin = lat_min, ymax = lat_max) +
-geom_point(data = casual_ride_data, aes(x = start_lng, y = start_lat), color = "red")# + 
+coord_map("albers",  lat0 = 45.75, lat1 = 41.64) +
+  geom_point(data = casual_ride_data, aes(x = 43.234, y = -80.42))
+#geom_point(data = casual_ride_data, aes(x = start_lng, y = start_lat)) + 
 #geom_point(data = member_ride_data, aes(x = start_lng, y = start_lat), color = "blue")
